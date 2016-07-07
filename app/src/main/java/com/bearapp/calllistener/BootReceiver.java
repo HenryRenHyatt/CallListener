30ac7a2308d7c521727b6469cc3f8079e68d0455
RenHaiLong <renhailong@RenHaiLong-MacBook-Pro.local>
2016-07-06 22:25:53 -0500

    增加开机启动功能

---
+++ app/src/main/java/com/bearapp/calllistener/BootReceiver.java
@@ -0,0 +1,18 @@
+package com.bearapp.calllistener;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    public BootReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Intent i = new Intent(context, CallListenerService.class);
+            context.startService(i);
+        }
+    }
+}
